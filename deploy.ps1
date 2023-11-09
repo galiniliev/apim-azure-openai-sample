f2bc4153bdac3de0811ed8ce64976d9cea9871e4
Galin Iliev <galini@microsoft.com>
2023-11-08 16:27:14 -0800

    add everything as parameters

---
+++ deploy.ps1
@@ -0,0 +1,5 @@
+$env:AZURE_OPENAI_KEY="your-key-here"
+$env:AZURE_OPENAI_ENDPOINT="https://devdiv-test-playground.openai.azure.com"
+$env:AZURE_OPENAI_MODEL="gpt-35"
+
+azd up -e azure-apim-francecentral
